5e05c7347fdccf0341ddd1cbff5eb9979016bc60
PancaGamaStore <108522948+PancaGamaStore@users.noreply.github.com>
2022-11-09 14:43:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6281251549899','6281251549899','6281251549899']
-global.premium = ['6281251549899']
+global.owner = ['6287734276016','6287734276016','6287734276016']
+global.premium = ['6287734276016']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
